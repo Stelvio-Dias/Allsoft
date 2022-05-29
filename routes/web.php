942e4932502e233362e5c89333396efdf74f7d7e
Stélvio Dias <mastervideo321@gmail.com>
2022-05-29 18:43:59 -0300

    Testando erro

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,6 @@ Route::get('pesquisa', 'PesquisaController@index')->name('pesquisa');
 
 
 // Footer action
-Route::post('newsletter', 'FooterController@newsletter')->name('newsletter');
 Route::post('mensagem', 'FooterController@mensagem')->name('mensagem');
 
 
